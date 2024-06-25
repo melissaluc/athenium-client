8bd672f094efe19550a0500b914f42c6abc13754
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-06-25 18:38:41 -0400

    repaired some functionality of strength page and workouts page

--- src/components/ExerciseStrengthLevel.jsx
+++ src/components/ExerciseStrengthLevel.jsx
@@ -23,40 +23,44 @@ function getProgressColour(strengthLevel, theme) {
         default:
             colour = null;
     }
-    console.log({strength: strengthLevel, colour: colour})
+    console.log({one_rep_max: strengthLevel, colour: colour})
     return colour;
 }
 
 function ExerciseStrengthLevel ({
                                 date_calculated,
                                 exercise_name,
-                                category,
+                                group,
                                 body_weight,
                                 relative_strength_demographic,
-                                relative_strength,
                                 one_rep_max,
-                                strength,
+                                reps,
+                                lift,
                                 strength_level,
                                 next_strength_level,
-                                strength_boundaries,
+                                strength_bounds,
+                                score,
+                                img_url,
                                 view}) {
                                 
     const theme = useTheme();
     const dateFormatted = new Date(date_calculated)
-    const progressValue = parseFloat(((strength - strength_boundaries[strength_level]) / (strength_boundaries[strength_level]) * 100).toFixed(1));
-
+    const progressValue = parseFloat(((one_rep_max - strength_bounds[strength_level]) / (strength_bounds[strength_level]) * 100).toFixed(1));
+    const relative_strength = one_rep_max/body_weight
     return(
         <Box>
         <Box sx={{ display: 'flex', justifyContent:'space-evenly'}}>
-            <Box sx={{backgroundColor:'gray', height:'auto', width:'4rem',aspectRatio:"1/1"}}>Img</Box>
+            <Box sx={{ border: '1px solid gray', height: '4rem', width: '4rem', aspectRatio: "1/1", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <img src={img_url} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} alt="Image" />
+            </Box>
             <Box>
-                {view && <Typography fontSize="0.7rem">{category}</Typography>}
+                {view && <Typography fontSize="0.7rem">{group}</Typography>}
                 <Typography>{exercise_name}</Typography>
                 <Typography fontSize="0.7rem">Updated on {dateFormatted.toLocaleDateString()}</Typography>
                 <Box sx={{ display: 'flex', justifyContent:'space-evenly', gap:"1rem"}}>
                     {/* <Typography fontSize=''>{`${relative_strength_demographic}% stronger than female lifters with similar stats`}</Typography> */}
                     <Box>
-                        <Typography>Your Lift is:</Typography>
+                        <Typography>Your Lift is {lift}lbs x {reps}</Typography>
                         <Typography>{relative_strength} times your body weight</Typography>
                     </Box>
 
@@ -76,9 +80,9 @@ function ExerciseStrengthLevel ({
             }}
                 >
                 <Typography>{strength_level}</Typography>
-                <Typography sx={{position: "relative", right:'-2rem'}}>{strength_level === "elite" ? null : (progressValue > 0 ? `${strength}(${progressValue}%)` : null)}</Typography>
+                <Typography sx={{position: "relative", right:'-2rem'}}>{strength_level === "elite" ? null : (progressValue > 0 ? `${one_rep_max}(${progressValue}%)` : null)}</Typography>
             </Box>
-            <Typography>{next_strength_level}</Typography>
+            <Typography>{next_strength_level ? next_strength_level : ""}</Typography>
         </Box>
     
         <LinearProgress
@@ -95,9 +99,9 @@ function ExerciseStrengthLevel ({
         />
         <Box sx={{display:'flex', justifyContent:'space-between'}}>
             {/* Value at current level */}
-            <Typography>{strength_boundaries[strength_level]}</Typography>
+            <Typography>{strength_bounds[strength_level]}</Typography>
             {/* Value at next level */}
-            <Typography>{strength_level==="elite"? strength :strength_boundaries[next_strength_level]}</Typography>
+            <Typography>{strength_level==="elite"? one_rep_max :strength_bounds[next_strength_level]}</Typography>
         </Box>
         </Box>
     </Box>
--- src/components/MuscleGroupStrengthCard.jsx
+++ src/components/MuscleGroupStrengthCard.jsx
@@ -1,12 +1,11 @@
 import { Box,  Typography, Card,CardContent, CardActionArea, CardActions, Button} from "@mui/material";
 import { useState } from 'react';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-
-import StrengthLevelExerciseList from "./StrengthLevelExerciseList";
+import ExerciseStrengthLevel from "./ExerciseStrengthLevel";
 import { useTheme } from "@emotion/react";
 
 
-function MuscleGroupStrengthCard ({muscleGroup, exercises}) {
+function MuscleGroupStrengthCard ({muscleGroup, exercises, groupScore}) {
     const theme = useTheme()
     const [expandWorkout, setExpandWorkout] = useState(false)
 
@@ -15,11 +14,49 @@ function MuscleGroupStrengthCard ({muscleGroup, exercises}) {
         <Card key={muscleGroup} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', width: '100%' }}>
             <CardContent>
                 <Box sx={{display:'flex', justifyContent:'space-around'}}>
-                    <Typography>Score</Typography>
+                    <Typography>{groupScore}</Typography>
                     <Typography>{muscleGroup}</Typography>
                 </Box>
                 {expandWorkout && 
-                <StrengthLevelExerciseList data={exercises}/>
+                
+                <>
+                {exercises.map((exercise)=>{
+                    const {
+                        date_calculated,
+                        exercise_name,
+                        group,
+                        body_weight,
+                        relative_strength_demographic,
+                        one_rep_max,
+                        reps,
+                        lift,
+                        strength_level,
+                        next_strength_level,
+                        strength_bounds,
+                        score,
+                        img_url
+                        } = exercise
+    
+                    return <ExerciseStrengthLevel
+                    key={exercise_name}
+                    date_calculated={date_calculated}
+                    exercise_name={exercise_name}
+                    group={group}
+                    body_weight={body_weight}
+                    relative_strength_demographic={relative_strength_demographic}
+                    one_rep_max={one_rep_max}
+                    reps={reps}
+                    lift={lift}
+                    strength_level={strength_level}
+                    next_strength_level={next_strength_level}
+                    strength_bounds={strength_bounds}
+                    score={score}
+                    img_url={img_url}
+                            />
+                })
+                }
+            </>            
+
                 }
             </CardContent>
             <CardActionArea sx={{
