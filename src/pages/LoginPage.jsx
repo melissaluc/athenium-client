1d6a8897fb13ccb663791a31668fbd20fb5f6874
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-08-01 16:37:40 -0400

    add login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,36 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { Box,Container } from '@mui/material';
 import AtheniumLogo from '../assets/AtheniumLogo';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 
 function LoginPage({}){
-
-
-    const responseMessage = (response) => {
-        console.log(response);
-    };
+    const navigate = useNavigate();
+    const handleLoginSuccess = (response) => {
+        // Send the Google token to your server
+        axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/google/login`, {
+          token: response.credential,
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((res) => {
+            // Handle successful login response from server
+            console.log(res.data); // axios automatically parses JSON
+            if(res.data.success) {
+                // Set authtoken
+                navigate(`/dashboard`)
+                localStorage.setItem('authToken', res.data.token);
+            }
+          })
+          .catch((error) => {
+            // Handle errors
+            console.error('Error:', error);
+          });
+      };
+    
     const errorMessage = (error) => {
         console.log(error);
     };
@@ -16,7 +38,7 @@ function LoginPage({}){
         <Container sx={{display:'flex', flexDirection:'column', width: "80vw"}}>
             <AtheniumLogo width={"10rem"}/>
             <Box sx={{width:"20vw"}}>
-                <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+                <GoogleLogin onSuccess={handleLoginSuccess} onError={errorMessage} />
             </Box>
         </Container>
     )
