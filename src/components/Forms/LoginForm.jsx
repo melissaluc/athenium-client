f7a35fbee8b667fcda4f2edc21c6dee85533dde4
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-08-03 17:42:30 -0400

    Add prelim login components

---
+++ src/components/Forms/LoginForm.jsx
@@ -0,0 +1,123 @@
+
+import { Box, TextField,FormControl, InputAdornment, Link, Button } from "@mui/material"
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import {useTheme} from "@mui/material";
+import {useState} from 'react'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+
+function LoginForm({errorMessage, handleLoginSuccess }){
+    const theme = useTheme()
+    const [visibility, setVisibility] = useState(false)
+    const [formData, setFormData] = useState({
+        username:'',
+        password:''
+    })
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleLoginSuccess(formData);
+    };
+
+    return(
+        <Box>
+            <FormControl 
+                onSubmit={handleSubmit} 
+                fullWidth 
+                sx={{display:'flex', flexDirection:'column', gap:'0.8rem', alignItems:'center'}}>
+                <TextField
+                    id="username"
+                    name="username"
+                    placeholder='Username'
+                    value={formData.username}
+                    variant="outlined"
+                    required
+                    onChange={handleChange}
+                    sx={{
+                        width:'100%',
+                        backgroundColor:'light blue',
+                        borderRadius: 2,
+                        '& .MuiInputBase-input': {
+                            borderRadius: 2,
+                        },
+                        '& .MuiInputBase-root': {
+                            '&:hover fieldset': {
+                                borderColor: `${theme.palette.primary.main}`, // Change border color on hover
+                            },
+                        },
+                    }}
+                    InputProps={{
+                        startAdornment: (
+                        <InputAdornment position="start">
+                        <PersonOutlineIcon/>
+                        </InputAdornment>
+                    ),
+                    }}
+                    />
+                <TextField
+                    id="password"
+                    name="password"
+                    placeholder='Password'
+                    value={formData.password}
+                    variant="outlined"
+                    required
+                    type={!visibility ? "password" : "text"}
+                    onChange={handleChange}
+                    sx={{
+                        width:'100%',
+                        borderRadius: 2,
+                        '& .MuiInputBase-input': {
+                            borderRadius: 2,
+                        },
+                        '& .MuiInputBase-root': {
+                            '&:hover fieldset': {
+                                borderColor: `${theme.palette.primary.main}`, // Change border color on hover
+                            },
+                        },
+                    }}
+                    InputProps={{
+                        startAdornment: (
+                        <InputAdornment position="start">
+                            <LockIcon /> 
+                        </InputAdornment>
+                    ),
+                        endAdornment: (
+                        <InputAdornment position="start" onClick={()=>{setVisibility(prev => !prev)}}>
+                            {!visibility  ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                        </InputAdornment>
+                    ),
+                    }}
+                />
+                        <Link
+                        // alignSelf='flex-end'
+                        href="#"
+                        variant="body2"
+                        sx={{ cursor: 'pointer', textDecoration: 'none' }}
+                        >
+                        Forgot Password?
+                        </Link>
+            </FormControl>
+            <Button 
+                onClick={handleSubmit} 
+                variant="contained"
+                onSuccess={handleSubmit} 
+                onError={errorMessage}
+                sx={{
+                    marginTop:'1rem', 
+                    width:"100%"}}
+                >Login</Button>
+        </Box>
+
+    )
+}
+
+export default LoginForm
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,47 @@
 import { GoogleLogin } from '@react-oauth/google';
-import { Box,Container } from '@mui/material';
+import { Box,Container, Typography, Divider, Link } from '@mui/material';
 import AtheniumLogo from '../assets/AtheniumLogo';
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
-
+import LoginForm from '../components/Forms/LoginForm'
+import { styled } from '@mui/material/styles';
 
 function LoginPage({}){
-    const navigate = useNavigate();
+    const navigate = useNavigate()
+
+    const Root = styled('div')(({ theme }) => ({
+      width: '100%',
+      ...theme.typography.body2,
+      color: theme.palette.text.secondary,
+      '& > :not(style) ~ :not(style)': {
+        marginTop: theme.spacing(2),
+      },
+    }));
+
     const handleLoginSuccess = (response) => {
+      // Send the Google token to your server
+      axios.post(`${process.env.REACT_APP_API_BASE_URL}/login`, response, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((res) => {
+          // Handle successful login response from server
+          console.log(res.data); // axios automatically parses JSON
+          if(res.data.success) {
+              // Set authtoken
+              
+              navigate(`/dashboard`)
+              localStorage.setItem('authToken', res.data.token);
+          }
+        })
+        .catch((error) => {
+          // Handle errors
+          console.error('Error:', error);
+        });
+    };
+
+    const handleGoogleLoginSuccess = (response) => {
         // Send the Google token to your server
         axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/google/login`, {
           token: response.credential,
@@ -35,10 +69,27 @@ function LoginPage({}){
         console.log(error);
     };
     return (
-        <Container sx={{display:'flex', flexDirection:'column', width: "80vw"}}>
-            <AtheniumLogo width={"10rem"}/>
-            <Box sx={{width:"20vw"}}>
-                <GoogleLogin onSuccess={handleLoginSuccess} onError={errorMessage} />
+        <Container sx={{display:'flex', flexDirection:'column',alignItems:'center' }}>
+            <Root sx={{display:'flex', flexDirection:'column', width: "70vw", alignItems:'center', margin:'10% 0%'}}>
+              <Box sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                <AtheniumLogo width={"1`00%"}/>
+                <Typography variant='h4' sx={{margin:'2rem', cursor:'default', userSelect: 'none',}}>LOGIN</Typography>
+                <LoginForm errorMessage={errorMessage} handleLoginSuccess={handleLoginSuccess}/>
+                <Divider sx={{ width: '90%', marginY: 2, cursor:'default', userSelect: 'none', }} orientation="horizontal" component="div" role="presentation" aria-hidden="true">Or Login with</Divider>
+
+              </Box>
+              <Box sx={{width:"20vw", display:'flex', justifyContent:'center', alignItems:'center'}}>
+                  <GoogleLogin onSuccess={handleGoogleLoginSuccess} onError={errorMessage} />
+              </Box>
+            </Root>
+            <Box>
+              <Link
+                href="#"
+                variant="body2"
+                sx={{ cursor: 'pointer', textDecoration: 'none' }}
+                >
+                Sign Up
+              </Link>
             </Box>
         </Container>
     )
